cc33826658ae27db45e95fa0342b138034a12b42
Tim Torgenrud <torg@stanford.edu>
2014-05-06 19:59:42 -0700

    updated index.html and TaskApp for simple task list

--- js/TaskApp.js
+++ js/TaskApp.js
@@ -1,8 +1,74 @@
 var TaskApp = function TaskApp () {
-  var listOfLists = ListOfLists(),
-      that = {};
+  var // listOfLists = ListOfLists(),
+    taskListUI = TaskListUI(),
+    taskList = TaskList({name: 'My Tasks'}),
+    that = {
+	
+	init: function init(taskList) {
+	    // load task lists on page
+	    $( document ).on("pagecreate", "#task-page", function () {
+		taskListUI.loadTaskList(taskList);
+	    });
+	}
+    };
 
-  // add functions to that
-
-  return that;
+    return that;
 }
+
+var TaskListUI = function TaskListUI () {
+    var taskList;
+
+    var  addTask = function addTask (task) {
+	var taskId = taskList.add(task);
+        displayTask(task, taskId);
+    },
+
+    displayTask = function displayTask (task, taskId) {
+        $('#task-list').append(
+                  [
+                      '<li id="', taskId, '">',
+                      '<a href="#">',
+                      task.name,
+                      '<br/>',
+                      task.details,
+                      '</a>',
+                      '<a href="#" class="delete">',
+                      'Delete',
+                      '</a>',
+                    '</li>'
+                  ].join('')
+        );
+        $('#' + taskId + ' .delete').on( "click", function () {
+            removeTask($(this).parent('li'));
+        });
+        $('#task-list').listview('refresh');
+    },
+
+    removeTask = function removeTask (taskItem) {
+        var taskId = taskItem.attr("id");
+                
+        if (taskList.remove(taskId)) {
+            taskItem.remove();
+            $('#task-list').listview('refresh');
+        }
+    },
+
+    loadTaskList(newTaskList) {
+	taskList = newTaskList;
+          
+	$("form :input").on("keypress", function(e) {
+            return e.keyCode != 13;
+	});
+
+	taskList.forEach(displayTask);
+
+	$( '#add' ).on("click", function () {
+            var taskname = $('#taskname').val(),
+            taskdetails = $('#taskdetails').val();
+            addTask({ name: taskname, details: taskdetails });
+            $( '#taskname' ).val('');
+            $( '#taskdetails' ).val('');
+	});
+    }
+});
+        
